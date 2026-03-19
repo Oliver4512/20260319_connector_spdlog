24e41a5cde15c1691a0dfee5bc98abba666bd608
Oliver A <oliver.hjemme@gmail.com>
2026-03-19 10:15:49 +0100

    cleaning

--- mariaDBinsert.cpp
+++ mariaDBinsert.cpp
@@ -6,46 +6,31 @@
 #include <string>
 
 #if __has_include("pass.hpp")
-    #include "pass.hpp"
+#include "pass.hpp"
 #else
-    std::string pass = "password";
+std::string pass = "password";
 #endif
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  std::ifstream f("pw.txt");
-  std::string pw;
-  if (f)
+
+  try
   {
-    std::ostringstream ss;
-    ss << f.rdbuf();
-    pw = ss.str();
-  }
-  else 
-  {
-    std::cerr << "Error: Could not open pw.txt" << std::endl;
-    //return 1;
-  }
-  
-  try 
-  {
-    sql::Driver* driver=sql::mariadb::get_driver_instance();
+    sql::Driver *driver = sql::mariadb::get_driver_instance();
     sql::SQLString url("jdbc:mariadb://127.0.0.1:3306/iot");
-    sql::Properties properties({{"user", "root"}, {"password", pw}});
+    sql::Properties properties({{"user", "root"}, {"password", pass}});
     std::unique_ptr<sql::Connection> conn(driver->connect(url, properties));
 
-    try 
+    try
     {
       conn->setAutoCommit(false);
       std::shared_ptr<sql::PreparedStatement> stmnt(conn->prepareStatement(
-            "INSERT INTO Devices(DeviceID, Temperatur) VALUES ('indoortemp', 30)"
-            )
-          );
+          "INSERT INTO devices(DeviceID, Temperatur) VALUES ('indoortemp', 30)"));
       stmnt->executeUpdate();
       conn->commit();
     }
 
-    catch(sql::SQLException &e) 
+    catch (sql::SQLException &e)
     {
       std::cerr << "Error updating contact with a transaction: " << e.what() << std::endl;
       conn->rollback();
@@ -53,7 +38,7 @@ int main (int argc, char *argv[])
     conn->close();
   }
 
-  catch (sql::SQLException& e) 
+  catch (sql::SQLException &e)
   {
     std::cerr << "Error connecting to the database: " << e.what() << std::endl;
     return 1;
