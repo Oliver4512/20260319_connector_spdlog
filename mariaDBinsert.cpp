9d68750cf07eb8c6cfd51f197bdce0b6b13330dd
Oliver A <oliver.hjemme@gmail.com>
2026-03-19 12:17:30 +0100

    example virker

--- mariaDBinsert.cpp
+++ mariaDBinsert.cpp
@@ -11,8 +11,43 @@
 std::string pass = "password";
 #endif
 
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h" // <-- til konsol
+#include <vector>
+
 int main(int argc, char *argv[])
 {
+  auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+      "logs/logexample", true); // true = overskriv ved opstart
+
+  spdlog::logger logger("logexample", {console_sink, file_sink});
+  spdlog::set_default_logger(std::make_shared<spdlog::logger>(logger));
+
+  spdlog::set_level(spdlog::level::debug);
+  spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] %v");
+
+  spdlog::info("Welcome to spdlog!");
+  spdlog::error("Some error message with arg: {}", 1);
+
+  spdlog::warn("Easy padding in numbers like {:08d}", 12);
+  spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+  spdlog::info("Support for floats {:03.2f}", 1.23456);
+  spdlog::info("Positional args are {1} {0}..", "too", "supported");
+  spdlog::info("{:<30}", "left aligned");
+
+  spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+  spdlog::debug("This message should be displayed..");
+
+  // change log pattern
+  spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+
+  // Compile time log levels
+  // Note that this does not change the current log level, it will only
+  // remove (depending on SPDLOG_ACTIVE_LEVEL) the call on the release code.
+  SPDLOG_TRACE("Some trace message with param {}", 42);
+  SPDLOG_DEBUG("Some debug message");
 
   try
   {
